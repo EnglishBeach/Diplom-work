dfc47aa4a9a9740c9104064c32b39e5dcd608fc3
STRANGER <kraynoff.i@yandex.ru>
2023-10-18 23:14:01 +0300

    fixes add recognize text

--- Video_process/test.py
+++ Video_process/test.py
@@ -7,14 +7,14 @@ from matplotlib.widgets import  Slider
 
 cap = cv2.VideoCapture("Video_process/Videos/Full_font1.avi")
 cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-ret, frame = cap.read()
+ret, BASE_FRAME = cap.read()
 
 
 
 # The parametrized function to be plotted
 def apply_thresh(thresh):
-    frame_2color = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    thresh, frame_bw = cv2.threshold(
+    frame_2color = cv2.cvtColor(BASE_FRAME, cv2.COLOR_BGR2GRAY)
+    _, frame_bw = cv2.threshold(
         frame_2color,
         thresh,
         255,
@@ -23,20 +23,20 @@ def apply_thresh(thresh):
     return frame_bw
 
 
-# Define initial parameters
+
 thresh = 0
 
-# Create the figure and the line that we will manipulate
+
 fig, ax = plt.subplots()
+
 frame = apply_thresh(thresh)
 image_plot = ax.imshow(frame)
-# image_plot.set_label(f'Recognize: {tes.image_to_string(frame)}')
+ax.text(5, 60, r'', fontsize=15)
+image_plot.set_xlabel(f'Recognize: {tes.image_to_string(frame)}')
 
-# adjust the main plot to make room for the sliders
 fig.subplots_adjust(top=1, hspace=0, bottom=0.25)
-
-# Make a horizontal slider to control the frequency.
 axfreq = fig.add_axes([0.2, 0.1, 0.6, 0.03])
+
 thresh_slider = Slider(
     ax=axfreq,
     label='Thresh',
@@ -52,7 +52,7 @@ def update(val):
 
     image_plot.autoscale()
     image_plot.set_data(frame)
-    # image_plot.set_label(f'Recognize: {tes.image_to_string(frame)}')
+    image_plot.set_xlabel(f'Recognize: {tes.image_to_string(frame)}')
 
     fig.canvas.draw_idle()
 
