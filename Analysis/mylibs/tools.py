b3b42d4fb6b01468a74f95eab7fc947dd536a2f9
STRANGER <kraynoff.i@yandex.ru>
2023-12-26 11:04:07 +0300

    general plots

--- Analysis/mylibs/tools.py
+++ Analysis/mylibs/tools.py
@@ -3,15 +3,17 @@ import copy
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 import statsmodels.api as sm
-from . import functions
+
+from . import functions, plots
+pd.set_option('mode.chained_assignment', None)
 
 def input_path(path=''):
     while (path == '') or (not os.path.isfile(path)):
         path = input(f"Input data path: ")
     return path
+
 
 def _split_path(path=''):
     path_list = (path).split('\\')
@@ -116,57 +118,6 @@ def regress(experiment: Experiment):
     return info, result, func
 
 
-# Plots and load
-pd.set_option('mode.chained_assignment', None)
-plt.rc('xtick', labelsize=10)
-plt.rc('ytick', labelsize=10)
-plt.rc('font', size=10)
-plt.rc('axes', titlesize=16)
-plt.rc('axes', labelsize=15)
-plt.rc('legend', fontsize=10)
-plt.rcParams["figure.figsize"] = (10, 7)
-
-VERBOSE_COLORS = {
-    'OK': 'g',
-    'OK_inner': 'b',
-    'image_sweep_check': 'r',
-    'combine_check': 'w',
-}
-
-
-def temporal_plot(
-    experiment,
-    title='',
-    ylabel='',
-    interactive=False,
-    save=False,
-):
-    fig, ax_v = plt.subplots()
-    ax_T = ax_v.twinx()
-    ax_v.scatter(experiment.d['time'], experiment.d['y'], color='red', marker='.')
-    ax_T.scatter(experiment.d['time'], experiment.d['x'], color='blue', marker='.')
-
-    fig.canvas.manager.set_window_title(title + ' plot')
-    fig.subplots_adjust(
-        top=0.9,
-        bottom=0.1,
-        left=0.1,
-        right=0.9,
-        hspace=0.2,
-        wspace=0.2,
-    )
-    ax_T.set_title(f"{experiment.name}: ({experiment.info['w']}% mass)")
-    ax_v.set_xlabel('Time [s]')
-    ax_T.set_ylabel('Temperature [C]', color='blue')
-    ax_v.set_ylabel(ylabel, color='red')
-
-    if interactive: plt.show()
-    if save:
-        os.makedirs(f'{experiment.folder}\Plots', exist_ok=True)
-        fig.savefig(f'{experiment.folder}\Plots\\{title}_{experiment.name}.jpg', dpi=600)
-    return experiment
-
-
 def _initial_filter(df, x=(-np.inf, np.inf), y=(0, np.inf), time=(0, np.inf)):
     temperature_cond = ((x[0] < df['x']) & (df['x'] < x[1]))
     viscosity_cond = ((y[0] < df['y']) & (df['y'] < y[1]))
@@ -204,7 +155,7 @@ def configurate_data(experiment: Experiment, save=False) -> Experiment:
         exp.d = _initial_filter(exp.d, time=time_lim, y=y_lim, x=(12, 42))
         exp.log.append(('initial_filter', {'time': time_lim, 'y': y_lim, 'x': (12, 42)}))
 
-        temporal_plot(
+        plots.temporal_plot(
             exp,
             title='Configurate',
             ylabel='Viscosity [cP]',
@@ -213,102 +164,3 @@ def configurate_data(experiment: Experiment, save=False) -> Experiment:
         )
         if _ask_continue(): break
     return exp
-
-
-def temperature_plot(
-    experiment: Experiment,
-    title='',
-    xlabel='',
-    ylabel='',
-    interactive=False,
-    save=False,
-):
-    fig, ax = plt.subplots()
-    colors = experiment.d['Viscosity_verbose'].replace(VERBOSE_COLORS)
-    ax.scatter(x=experiment.d['x'], y=experiment.d['y'], c=colors, s=5)
-    sns.lineplot(
-        ax=ax,
-        data=experiment.d,
-        x='x',
-        y='y',
-        estimator='mean',
-        errorbar=("sd", 1),
-        label='mean',
-    )
-    sns.lineplot(
-        ax=ax,
-        data=experiment.d,
-        x="x",
-        y="y",
-        errorbar=('pi', 50),
-        estimator="median",
-        label='median',
-    )
-
-    fig.canvas.manager.set_window_title(title + ' plot')
-    fig.subplots_adjust(
-        top=0.9,
-        bottom=0.1,
-        left=0.1,
-        right=0.9,
-        hspace=0.2,
-        wspace=0.2,
-    )
-    ax.set_title(f"{experiment.name}: ({experiment.info['w']}% mass)")
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
-
-    if interactive: plt.show()
-    if save:
-        os.makedirs(f'{experiment.folder}\Plots', exist_ok=True)
-        fig.savefig(f'{experiment.folder}\Plots\\{title}_{experiment.name}.jpg', dpi=600)
-
-
-def comparation_plot(
-    experiment: Experiment,
-    ols_exp: Experiment,
-    title,
-    xlabel,
-    ylabel,
-    interactive=False,
-    save=False,
-):
-    fig, ax = plt.subplots()
-    colors = experiment.d['Viscosity_verbose'].replace(VERBOSE_COLORS)
-    ax.scatter(
-        experiment.d['x'],
-        experiment.d['y'],
-        color=colors,
-        marker='.',
-        label='Data',
-        alpha=0.6,
-    )
-
-    E = experiment.info['E']
-    D0 = experiment.info['D0']
-    ax.plot(
-        ols_exp.d['x'],
-        ols_exp.d['y'],
-        color='black',
-        label=f'OLS: \nE= {E/1000: >8.2f} kJ\nD= {D0: >8.2e} m2/s',
-    )
-
-    fig.canvas.manager.set_window_title(title + ' plot')
-    fig.subplots_adjust(
-        top=0.9,
-        bottom=0.1,
-        left=0.1,
-        right=0.9,
-        hspace=0.2,
-        wspace=0.2,
-    )
-
-    ax.set_title(f"{experiment.name}: ({experiment.info['w']}% mass)")
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
-    ax.legend()
-
-    if interactive: plt.show()
-    if save:
-        os.makedirs(f'{experiment.folder}\Plots', exist_ok=True)
-        fig.savefig(f'{experiment.folder}\Plots\\{title}_{experiment.name}.jpg', dpi=600)
