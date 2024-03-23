0b7da5fa44d0418b6f1ebc20f711be04e091c8aa
STRANGER <kraynoff.i@yandex.ru>
2024-03-23 16:55:25 +0300

    add polymerization

--- COMSOL/Comparation/tools.py
+++ COMSOL/Comparation/tools.py
@@ -41,6 +41,7 @@ class Solver:
         self.comp = {}
         for i, key in enumerate(comps.split(', ')):
             self.comp[key] = i
+        self.solve()
 
     def solve(self, initial=None, K=None):
         init = initial if initial is not None else self.initial
@@ -68,7 +69,7 @@ class Differ:
 
     def __getitem__(self, comp):
         sigma = 1e-9
-        return (self.y1[comp] - self.y2[comp]) / (self.y1[comp].mean() + sigma) * 100
+        return self.y1[comp] - self.y2[comp]
 
     def solve(self, initial=None, K=None):
         self.y1.solve(initial=initial, K=K)
@@ -81,7 +82,6 @@ class Comparator:
         self.y2: Solver = y2
         self.K = y1.K
         self.T = y1.T
-        self.y1.solve()
 
     def __getitem__(self, comp):
         sigma = 1e-9
